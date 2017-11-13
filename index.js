73ac7920debc87cb832e0f4f569411a32e3f07ab
Andrew Rossman <andrew100r@gmail.com>
2017-11-13 15:09:34 -0800

    implements player move

--- index.js
+++ index.js
@@ -3,47 +3,57 @@ prompt.start();
 
 const displayBoard = (board) => {
   console.log('', board[0][0], '|', board[0][1], '|', board[0][2]);
-  console.log('--------');
+  console.log('-----------');
   console.log('', board[1][0], '|', board[1][1], '|', board[1][2]);
-  console.log('--------');
+  console.log('-----------');
   console.log('', board[2][0], '|', board[2][1], '|', board[2][2]);
 };
 
 const play = (board, move) => {
   displayBoard(board);
+  let row = null;
+  let col = null;
   console.log('Select a cell on the board, 1 through 9: ');
   prompt.get(['cell'], (err, result) => {
     if (!err) {
-      if (result.cell === 1) {
-        const row = 0;
-        const col = 0;
-      } else if(result.cell === 2) {
-        const row = 0;
-        const col = 1;
-      } else if(result.cell === 3) {
-        const row = 0;
-        const col = 2;
-      } else if(result.cell === 4) {
-        const row = 1;
-        const col = 0;
-      } else if(result.cell === 5) {
-        const row = 1;
-        const col = 1;
-      } else if(result.cell === 6) {
-        const row = 1;
-        const col = 2;
-      } else if(result.cell === 7) {
-        const row = 2;
-        const col = 0;
-      } else if(result.cell === 8) {
-        const row = 2;
-        const col = 1;
-      } else if(result.cell === 9) {
-        const row = 2;
-        const col = 2;
+      if (result.cell === '1') {
+        row = 0;
+        col = 0;
+      } else if(result.cell === '2') {
+        row = 0;
+        col = 1;
+      } else if(result.cell === '3') {
+        row = 0;
+        col = 2;
+      } else if(result.cell === '4') {
+        row = 1;
+        col = 0;
+      } else if(result.cell === '5') {
+        row = 1;
+        col = 1;
+      } else if(result.cell === '6') {
+        row = 1;
+        col = 2;
+      } else if(result.cell === '7') {
+        row = 2;
+        col = 0;
+      } else if(result.cell === '8') {
+        row = 2;
+        col = 1;
+      } else if(result.cell === '9') {
+        row = 2;
+        col = 2;
       } else {
         console.log('Invalid selection.')
         play(board, move);
+      }
+      if (board[row][col] === ' ') {
+        board[row][col] = move;
+        const newMove = move === 'x' ? 'o' : 'x';
+        play(board, newMove);
+      } else {
+        console.log('Invalid selection.')
+        play(board, move); 
       }
     } else {
       console.log('Error getting your input');
@@ -53,9 +63,9 @@ const play = (board, move) => {
 
 (() => {
   const board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
   ];
   let move = 'x';
   play(board, move);
